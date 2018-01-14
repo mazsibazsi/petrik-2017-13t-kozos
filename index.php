dc08bd319dd52c0cefbe955ac55a98a4f698a5fa
Unknown <mahli@posteo.net>
2018-01-14 11:14:37 +0100

    hf colorpicker bg

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Kolorpikkeres háttérszínváltoztatós
+	</title>
+	<script src="jscolor.js"></script>
+	</head>
+<body>
+	<button class="jscolor {onFineChange:'update(this)'}"  value="FFFFFF" ></button>
+	<?php
 
-$nev = 'ゲットだぜ';
-$datum = new \DateTime();
-$datumStr = $datum->format('Y-m-d\TH:i:s');
-$szin = '#dd22dd';
+	$nev = 'ゲットだぜ';
+	$datum = new \DateTime();
+	$datumStr = $datum->format('Y-m-d\TH:i:s');
+	$szin = '#dd22dd';
 
-require 'contents.php';
+	require 'contents.php';
+	?>
+</body>
+<script>
+function update(jscolor) {
+    // 'jscolor' instance can be used as a string
+    document.body.style.backgroundColor = '#' + jscolor;
+}
+</script>
+</html>
+
+
